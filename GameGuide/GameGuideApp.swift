dc8f7421ac330a3cbdf682492a02567d89522851
poz-dev <kresimir.ivanjko@icloud.com>
2024-01-03 19:48:59 +0100

    Initial Commit

---
+++ GameGuide/GameGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameGuideApp.swift
+//  GameGuide
+//
+//  Created by Kresimir Ivanjko on 03.01.2024..
+//
+
+import SwiftUI
+
+@main
+struct GameGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
